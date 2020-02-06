6875861b001f3545fa982fb9b6fc98ecdce0091f
medioxumate <[briankiehn@comcast.net]>
2020-02-05 20:04:06 -0500

    fixed issues with search function. Added more JSON objects.

--- json-search.js
+++ json-search.js
@@ -39,25 +39,79 @@ janeR = {
         "last" : "de Milliano"
     },
     "Mother" : {
+        "first" : "Patli",
+        "last" : "Ramirez"
+    }
+};
+
+petrusM = {
+    "Name": {
+        "first" : "Petrus",
+        "last" : "de Milliano"
+    },
+    "Sex": "m",
+    "Born" : 1858,
+    "Died" : 1895,
+    "Father" : {
+        "first" : "Escobar",
+        "last" : "de Milliano"
+    },
+    "Mother" : {
         "first" : "Isabel",
         "last" : "Nunez"
     }
 };
 
-let list = {janeD, janeR};
+ottoG = {
+    "Name": {
+        "first" : "Otto",
+        "last" : "the Great"
+    },
+    "Sex": "m",
+    "Born" : 912,
+    "Died" : 973,
+    "Father" : {
+        "first" : "Henry",
+        "last" : "the Fowler"
+    },
+    "Mother" : {
+        "first" : "Mathilde",
+        "last" : "of Enger"
+    }
+};
+
+catA = {
+    "Name": {
+        "first" : "Catherine",
+        "last" : " of Aragon"
+    },
+    "Sex": "f",
+    "Born" : 1485,
+    "Died" : 1536,
+    "Father" : {
+        "first" : "Ferdinand",
+        "last" : "of Aragon"
+    },
+    "Mother" : {
+        "first" : "Isabella",
+        "last" : "of Castile"
+    }
+};
+
+let list = [janeD, janeR, petrusM, ottoG, catA];
 
 function search(){
-    let string = document.getElementById("search").value;
+    let string = document.getElementById("search").value.toString();
     document.getElementById("print").innerHTML = "";
 
     let success = false;
 
-    if(string !== "") {
-        let reg = new RegExp(string, "i");
-        for (let query in list) {
+    if(!(string === "")) {
+        for (let i = 0; i < list.length; i++) {
+            let comp = compare(list[i]);
 
-            if (query.search(reg) !== -1) {
-                printJSON(query);
+            if(comp.toLowerCase().includes(string.toLowerCase())) {
+                printJSON(list[i]);
                 success = true;
             }
         }
@@ -70,6 +124,11 @@ function search(){
     }
 }
 
+function compare(object){
+    return object.Name.first+" "+object.Name.last+" : "+ object.Sex +" : " + object.Born+ " : " + object.Died +" : "
+    + object.Father.first+" "+object.Father.last+" : "+ object.Mother.first+" "+ object.Mother.last;
+}
+
 function printJSON(object) {
         document.getElementById("print").innerHTML += "<li>Name: "+
             object.Name.first+" "+object.Name.last+"</li>";
@@ -79,5 +138,5 @@ function printJSON(object) {
         document.getElementById("print").innerHTML += "<li>Father: "+
             object.Father.first+" "+object.Father.last+"</li>";
         document.getElementById("print").innerHTML += "<li>Mother: "+
-            object.Mother.first+" "+ object.Mother.last+"</li>";
+            object.Mother.first+" "+ object.Mother.last+"</li><br>";
 }
